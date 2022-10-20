19c7094ca07e5ef909f6bfbc353636dce90e7363
PizzzaDog <7883494@gmail.com>
2022-10-20 22:07:57 +0400

    added true/false security

--- src/main/java/controller/SecurityController.java
+++ src/main/java/controller/SecurityController.java
@@ -2,6 +2,13 @@ package controller;
 
 public class SecurityController {
 
+    public boolean isSecure2() {
+        if (1 < 10) {
+            return true;
+        }
+        return false;
+    }
+
     public boolean isSecure() {
         return true;
     }
